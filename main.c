f0233d3cf4d514a841d59dff332f9777d529655b
Sergey Khalzov <khalzovsergey@mail.ru>
2013-11-14 20:10:58 +0400

    Рабочий вариант сервера. Добавил обработку запроса.

--- main.c
+++ main.c
@@ -1,24 +1,13 @@
-// printf
 #include <stdio.h>
-// malloc
 #include <stdlib.h>
-
-
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-
-// struct sockaddr_un
-//#include <sys/un.h>
-
 #include <signal.h>
 #include <unistd.h>
-
-
-// IPPORT_RESERVED
-//#include <netdb.h>
-
-// struct sockaddr_in
+#include <arpa/inet.h>
 #include <netinet/in.h>
+#include <fcntl.h>
 
 
 #define _DEBUG
@@ -26,7 +15,6 @@
 #ifdef _DEBUG
 
 #define TRACE printf("%s; %d\n", __FILE__, __LINE__);
-//#define TRACE printf("main.c; %d\n", __LINE__);
 
 #else
 
@@ -36,16 +24,29 @@
 
 
 #define SIZE_OF_QUEUE 10
-#define INPORT 9998
+#define INPORT 1025
 #define TRUE 1
 #define FALSE 0
+#define INADDR "127.0.0.1"
+// размер буфера
+#define BUF_SIZE 4096
 
 
-int process_request(int client_sockfd);
+// обработать запрос
+int process_query(int client_sockfd);
+// закрыть сервер
+void close_server(int sig);
 
 
 int main(int avgc, char *argv[])
 {
+    // закрытие сервера
+    struct sigaction act;
+    act.sa_handler = close_server;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGINT, &act, 0);
+
     // объвления переменных для сервера
     int server_sockfd;
     struct sockaddr_in server_address;
@@ -61,13 +62,13 @@ int main(int avgc, char *argv[])
 
     // именовать сокет
     server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_addr.s_addr = inet_addr(INADDR);
     server_address.sin_port = htons(INPORT);
     if (bind(server_sockfd, (struct sockaddr *)&server_address, (socklen_t)sizeof(server_address)))
     {
         // напечатать сообщение об ошибке именования сокета
         perror("server bind error");
-        close(server_sockfd);
+        //close(server_sockfd);
         exit(EXIT_FAILURE);
     }
 
@@ -76,180 +77,134 @@ int main(int avgc, char *argv[])
     {
         // напечатать сообщение об ошибке создания очереди соединений
         perror("server listen error");
-        close(server_sockfd);
+        //close(server_sockfd);
         exit(EXIT_FAILURE);
     }
 
     // игнорировать статус завершения дочернего процесса для устранения появления зомби-процессов
     signal(SIGCHLD, SIG_IGN);
 
-    // счетчик ветвлений
-    int fork_count = 0;
-    // счетчик неудачных ветвлений
-    int fork_error_count = 0;
-
     // объвления переменных для клиента
     int client_sockfd;
     socklen_t client_len;
     struct sockaddr_in client_address;
 
+    // номер запроса
+    int query_number = 0;
+
     // цикл обработки запросов
+    printf("server waiting...\n");
     while (TRUE)
     {
         client_len = sizeof(client_address);
         client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &client_len);
-        fork_count++;
+        printf("query number = %d\n", query_number++);
         switch (fork())
         {
             case -1:
                 // напечатать сообщение об ошибке ветвления
                 perror("server fork error");
-                // увеличить счетчик ошибок ветвления
-                fork_error_count++;
-                // обработать клиента в головном процессе
-                process_request(client_sockfd);
+                // закрыть клиентский сокет
+                close(client_sockfd);
                 break;
             case 0:
                 // обработать клиента в дочернем процессе процессе
-                exit(process_request(client_sockfd));
+                exit(process_query(client_sockfd));
                 break;
             default:
+                // закрыть клиентский сокет
                 close(client_sockfd);
                 break;
         }
     }
 
-    // закрыть слушающий сокет
-    //close(server_sockfd);
-    // завершить программу
-    //exit(EXIT_SUCCESS);
-
-/*
-    // максимальный порт
-    //printf("%d\n", IPPORT_RESERVED);
-
-    // неблокирующее ожидание
-    //int flags = fcntl(socket, F_GETFL, 0);
-    //fnctl(socket, F_SETFL, O_NONBLOCK | flags);
-
-    int server_sockfd, client_sockfd;
-    socklen_t server_len, client_len;
-    struct sockaddr_un server_address;
-    struct sockaddr_un client_address;
-
-    // удалить старый сокет
-    unlink("server_socket");
-
-    // создать слушающий сокет
-    server_sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-
-    // присвоить имя сокету
-    server_address.sun_family = AF_UNIX;
-    strcpy(server_address.sun_path, "server_socket");
-    server_len = sizeof(server_address);
-    bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
-
-    // создать очередь запросов
-    listen(server_sockfd, SIZE_OF_QUEUE);
-
-    // работа с клиентами
-    int count = 10;
-    while (count--)
-    {
-        char ch;
-        printf("server waiting...\n");
-        client_len = sizeof(client_address);
-        client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &client_len);
-        read(client_sockfd, &ch, sizeof(char));
-        ch++;
-        write(client_sockfd, &ch, sizeof(char));
-        close(client_sockfd);
-    }
-*/
-
-/*
-    int sockfd;
-    int len;
-    struct sockaddr_un address;
-    int result;
-    char ch = 'A';
-
-    // создать сокет
-    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-
-    // присвоить имя сокету
-    address.sun_family = AF_UNIX;
-    strcpy(address.sun_path, "server_socket");
-    len = sizeof(address);
-
-    // подключиться к серверу
-    result = connect(sockfd, (struct sockaddr *)&address, len);
-    if (result == -1)
-    {
-        perror("connection error");
-        exit(EXIT_FAILURE);
-    }
-
-    // обмен данными с сокетом
-    // послать данные на сервер
-    write(sockfd, &ch, sizeof(char));
-    // получить данные от сервера
-    read(sockfd, &ch, sizeof(char));
-    printf("char from server = %c\n", ch);
-
-    // закрыть сокет
-    close(sockfd);
-
-
-
-    TRACE
-*/
-
-    //printf("Hello world!\n");
     //exit(EXIT_FAILURE);
     //exit(EXIT_SUCCESS);
 }
 
 
-#define BUF_SIZE 4096
-
-
-int process_request(int client_sockfd)
+// копировать данные из одного файлового дескриптора в другой файловый дескриптор
+int copy(int fd_sour, int fd_dest)
 {
-    char *buf[BUF_SIZE];// = (char *)malloc(BUF_SIZE * sizeof(char));
+    void *buf[BUF_SIZE];
+    int count;
     int result = 0;
-    int count = 0;
-    while (count <= BUF_SIZE && (result = read(client_sockfd, buf + count, BUF_SIZE - count)))
+    while (TRUE)
     {
-        if (result == -1)
+        count = read(fd_sour, buf, BUF_SIZE);
+        if (count <= 0)
         {
-            close(client_sockfd);
-            return EXIT_FAILURE;
+            break;
         }
-        count += result;
+        count = write(fd_dest, buf, count);
+        if (count <= 0)
+        {
+            break;
+        }
+        result += count;
+    }
+    return result;
+}
+
+
+#define METHOD_GET "GET"
+#define METHOD_NOT_IMPLEMENTED "HTTP/1.1 501 Not Implemented\r\n\r\n<html><body><h1>501 Not Implemented</h1></body></html>"
+#define ROOT "/home/sergey"
+#define INDEX "/home/sergey/index.html"
+#define OK "HTTP/1.1 200 OK\r\n\r\n"
+#define NOT_FOUND "HTTP/1.1 404 Not Found\r\n\r\n<html><body><h1>404 Not Found</h1></body></html>"
+
+
+int process_query(int client_sockfd)
+{
+    char buf[BUF_SIZE];
+
+    int count = read(client_sockfd, buf, BUF_SIZE - 1);
+    if (count <= 0)
+    {
+        return EXIT_FAILURE;
     }
 
+    buf[count] = '\0';
 
+    char *method = strtok(buf, " ");
+    char *uri = strtok(NULL, " ");
 
-    //free(buf);
-    close(client_sockfd);
-    //return EXIT_FAILURE;
+    if (strcmp(method, METHOD_GET) == 0)
+    {
+        int fd;
+        if (strcmp(uri, "/") == 0)
+        {
+            fd = open(INDEX, O_RDONLY);
+        }
+        else
+        {
+            char path[BUF_SIZE];
+            strcpy(path, ROOT);
+            strcpy(path + sizeof(ROOT) - 1, uri);
+            fd = open(path, O_RDONLY);
+        }
+        if (fd >= 0)
+        {
+            write(client_sockfd, OK, sizeof(OK) - 1);
+            copy(fd, client_sockfd);
+        }
+        else
+        {
+            write(client_sockfd, NOT_FOUND, sizeof(NOT_FOUND) - 1);
+        }
+    }
+    else
+    {
+        write(client_sockfd, METHOD_NOT_IMPLEMENTED, sizeof(METHOD_NOT_IMPLEMENTED) - 1);
+    }
+
     return EXIT_SUCCESS;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void close_server(int sig)
+{
+    printf("\nserver is closed\n");
+    exit(EXIT_SUCCESS);
+}
